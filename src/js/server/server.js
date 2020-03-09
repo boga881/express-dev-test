88f97146a334a95368d1d6a445b5d13e5a0ef654
Daniel Baranowski <>
2020-03-10 09:07:16 +1100

    - add onoff

--- src/js/server/server.js
+++ src/js/server/server.js
@@ -34,6 +34,12 @@ if (devServerEnabled) {
 app.use(express.static('./dist'));
 
 app.post('/api/pew', multipart.any(), function (req, res) {
+  const Gpio = require('onoff').Gpio;
+  const led = new Gpio(17, 'out');
+  const button = new Gpio(4, 'in', 'both');
+
+  button.watch((err, value) => led.writeSync(value));
+
  res.json('pew pew');
 });
 
